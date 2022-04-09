f0cfe7aeda7fe3c4e216fd762edf59a5731f936b
Isaac Kilbourne <kilbouri@uwindsor.ca>
2022-04-09 19:04:27 -0400

    Update data and add .prettierrc

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 This repo contains the source code and associated files for a backend that sits between our MySQL database and frontend web application. There is little to no security in use because this project is only intended to be used for a couple days.
 
 TL;DR: Don't use this as anything other than reference.
+
+## Running:
+
+First,
+
+```sh
+# Install dependencies
+yarn
+
+# Start server
+yarn start
+```
--- server.js
+++ server.js
@@ -2,23 +2,23 @@
  * Receive a plain text SQL query and return the result as a JSON object.
  */
 
-const mysql = require('mysql');
-const express = require('express');
-const cors = require('cors');
-const util = require('util');
+const mysql = require("mysql");
+const express = require("express");
+const cors = require("cors");
+const util = require("util");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: '',
-    password: '',
-    database: 'powerCompany'
+    host: "localhost",
+    user: "",
+    password: "",
+    database: "powerCompany",
 });
 
-app.get('/', async function (req, res) {
+app.get("/", async function (req, res) {
     // receive a plain text SQL query, execute it, and return the result as a JSON object
     console.log(`Received query "${req.query.sql}"`);
 
@@ -26,27 +26,27 @@ app.get('/', async function (req, res) {
     // we do .bind(connection) because connection.query depends on this being the connection
     const query = util.promisify(connection.query).bind(connection);
 
-     let result;
-     try { 
-         result = await query(req.query.sql);
-     } catch (err) {
-          res.status(400).send(err);
-          return;
-     }
-     res.status(200).json(result);
+    let result;
+    try {
+        result = await query(req.query.sql);
+    } catch (err) {
+        res.status(400).send(err);
+        return;
+    }
+    res.status(200).json(result);
 });
 
 const port = process.env.PORT || 6969;
 const server = app.listen(port, function () {
-    console.log('Server is running on port ' + port);
+    console.log("Server is running on port " + port);
 });
 
 // graceful shutdown should close the database connection
 const shutdownHandler = (e) => {
-    console.log('\nAbout to exit, closing database connections');
+    console.log("\nAbout to exit, closing database connections");
     connection.end();
     process.exit(0);
 };
 
-process.on('exit', shutdownHandler);
-process.on('SIGINT', shutdownHandler);
+process.on("exit", shutdownHandler);
+process.on("SIGINT", shutdownHandler);
